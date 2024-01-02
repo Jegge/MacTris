678877af721df2c3219814cbaba4bbe3f32e0bb8
Sebastian Boettcher <jegge@gmx.net>
2024-01-02 08:44:31 +0100

    Initial Commit

---
+++ Mactris/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Mactris
+//
+//  Created by Sebastian Boettcher on 02.01.24.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
