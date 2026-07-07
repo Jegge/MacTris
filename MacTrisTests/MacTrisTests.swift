698a93d8834aaaa626fd28407ed152deef29b620
Sebastian Boettcher <jegge@gmx.net>
2026-07-07 18:25:44 +0200

    Added testing target

---
+++ MacTrisTests/MacTrisTests.swift
@@ -0,0 +1,19 @@
+//
+//  MacTrisTests.swift
+//  MacTrisTests
+//
+//  Created by Sebastian Boettcher on 07.07.26.
+//
+
+import Testing
+
+struct MacTrisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+     //   #expect(false)
+    }
+
+}
