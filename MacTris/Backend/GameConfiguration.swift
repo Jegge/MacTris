e6f8fc8f0473e85fdf90bbd6fe5b9a07ff235a00
Sebastian Boettcher <jegge@gmx.net>
2026-07-10 20:08:31 +0200

    Readded new color scheme, but as separate option

--- MacTris/Backend/GameConfiguration.swift
+++ MacTris/Backend/GameConfiguration.swift
@@ -41,13 +41,14 @@ extension RandomGeneratorMode: CustomStringConvertible {
 enum Appearance: Int {
     case plain = 1
     case shaded = 2
+    case bright = 3
 
     func increase() -> Appearance {
         return Appearance(rawValue: self.rawValue + 1) ?? .plain
     }
 
     func decrease() -> Appearance {
-        return Appearance(rawValue: self.rawValue - 1) ?? .shaded
+        return Appearance(rawValue: self.rawValue - 1) ?? .bright
     }
 }
 
@@ -56,6 +57,7 @@ extension Appearance: CustomStringConvertible {
         switch self {
         case .plain: return "Plain"
         case .shaded: return "Shaded"
+        case .bright: return "Bright"
         }
     }
 }
