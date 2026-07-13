5617350a348f67cb4400bc31fba87bc79b075144
Sebastian Boettcher <jegge@gmx.net>
2026-07-13 21:59:37 +0200

    Fixed potential crash if app version would not have a minor part

--- MacTris/Extensions/Bundle+Properties.swift
+++ MacTris/Extensions/Bundle+Properties.swift
@@ -11,7 +11,7 @@ extension Bundle {
     var version: AppVersion {
         let bundleVersion = (self.infoDictionary?["CFBundleShortVersionString"] as? String) ?? "0.0"
         let parts = bundleVersion.split(separator: ".").map { Int(String($0)) ?? 0 }
-        return AppVersion(major: parts[0], minor: parts[1])
+        return AppVersion(major: parts.first ?? 0, minor: parts.count > 1 ? parts[1] : 0)
     }
 
     var build: Int {
