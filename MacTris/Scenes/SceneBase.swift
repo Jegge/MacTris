31ff4e7a75f2b3d8dcf7eeac695f6da15324c9ae
Sebastian Boettcher <jegge@gmx.net>
2026-07-14 23:35:49 +0200

    Cleaned up observer handling

--- MacTris/Scenes/SceneBase.swift
+++ MacTris/Scenes/SceneBase.swift
@@ -8,14 +8,8 @@
 import SpriteKit
 
 class SceneBase: SKScene {
-    private var inputDownObserver: NSObjectProtocol?
-    private var inputUpObserver: NSObjectProtocol?
-    private var controllerDidConnectObserver: NSObjectProtocol?
-    private var controllerDidDisconnectObserver: NSObjectProtocol?
-    private var didEnterFullScreenObserver: NSObjectProtocol?
-    private var didExitFullScreenObserver: NSObjectProtocol?
-    private var didResignKeyObserver: NSObjectProtocol?
 
+    private var observers: [NSObjectProtocol] = []
     private var eventMonitor: Any?
 
     // swiftlint:disable:next implicitly_unwrapped_optional
@@ -37,37 +31,33 @@ class SceneBase: SKScene {
     override func didMove(to view: SKView) {
         super.didMove(to: view)
 
-        self.inputDownObserver = NotificationCenter.default.addObserver(forName: InputEvent.inputDownNotification, object: nil, queue: .main) { [weak self] notification in
-            if let event = notification.object as? InputEvent {
-                self?.inputDown(event: event)
+        self.observers = [
+            NotificationCenter.default.addObserver(forName: InputEvent.inputDownNotification, object: nil, queue: .main) { [weak self] notification in
+                if let event = notification.object as? InputEvent {
+                    self?.inputDown(event: event)
+                }
+            },
+            NotificationCenter.default.addObserver(forName: InputEvent.inputUpNotification, object: nil, queue: .main) { [weak self] notification in
+                if let event = notification.object as? InputEvent {
+                    self?.inputUp(event: event)
+                }
+            },
+            NotificationCenter.default.addObserver(forName: NSNotification.Name.GCControllerDidConnect, object: nil, queue: .main) { [weak self] _ in
+                self?.controllerDidConnect()
+            },
+            NotificationCenter.default.addObserver(forName: NSNotification.Name.GCControllerDidDisconnect, object: nil, queue: .main) { [weak self] _ in
+                self?.controllerDidDisconnect()
+            },
+            NotificationCenter.default.addObserver(forName: NSWindow.didEnterFullScreenNotification, object: nil, queue: .main) { [weak self] _ in
+                self?.didEnterFullScreen()
+            },
+            NotificationCenter.default.addObserver(forName: NSWindow.didExitFullScreenNotification, object: nil, queue: .main) { [weak self] _ in
+                self?.didExitFullScreen()
+            },
+            NotificationCenter.default.addObserver(forName: NSWindow.didResignKeyNotification, object: nil, queue: .main) { [weak self] _ in
+                self?.didResignKey()
             }
-        }
-
-        self.inputUpObserver = NotificationCenter.default.addObserver(forName: InputEvent.inputUpNotification, object: nil, queue: .main) { [weak self] notification in
-            if let event = notification.object as? InputEvent {
-                self?.inputUp(event: event)
-            }
-        }
-
-        self.controllerDidConnectObserver = NotificationCenter.default.addObserver(forName: NSNotification.Name.GCControllerDidConnect, object: nil, queue: .main) { [weak self] _ in
-            self?.controllerDidConnect()
-        }
-
-        self.controllerDidDisconnectObserver = NotificationCenter.default.addObserver(forName: NSNotification.Name.GCControllerDidDisconnect, object: nil, queue: .main) { [weak self] _ in
-            self?.controllerDidDisconnect()
-        }
-
-        self.didEnterFullScreenObserver = NotificationCenter.default.addObserver(forName: NSWindow.didEnterFullScreenNotification, object: nil, queue: .main) { [weak self] _ in
-            self?.didEnterFullScreen()
-        }
-
-        self.didExitFullScreenObserver = NotificationCenter.default.addObserver(forName: NSWindow.didExitFullScreenNotification, object: nil, queue: .main) { [weak self] _ in
-            self?.didExitFullScreen()
-        }
-
-        self.didResignKeyObserver = NotificationCenter.default.addObserver(forName: NSWindow.didResignKeyNotification, object: nil, queue: .main) { [weak self] _ in
-            self?.didResignKey()
-        }
+        ]
 
         self.eventMonitor = NSEvent.addLocalMonitorForEvents(matching: .flagsChanged, handler: eventFlagsChanged(event:))
     }
@@ -88,27 +78,11 @@ class SceneBase: SKScene {
     override func willMove(from view: SKView) {
         super.willMove(from: view)
 
-        if let observer = self.inputDownObserver {
+        self.observers.forEach { observer in
             NotificationCenter.default.removeObserver(observer)
         }
-        if let observer = self.inputUpObserver {
-            NotificationCenter.default.removeObserver(observer)
-        }
-        if let observer = self.controllerDidConnectObserver {
-            NotificationCenter.default.removeObserver(observer)
-        }
-        if let observer = self.controllerDidDisconnectObserver {
-            NotificationCenter.default.removeObserver(observer)
-        }
-        if let observer = self.didEnterFullScreenObserver {
-            NotificationCenter.default.removeObserver(observer)
-        }
-        if let observer = self.didExitFullScreenObserver {
-            NotificationCenter.default.removeObserver(observer)
-        }
-        if let observer = self.didResignKeyObserver {
-            NotificationCenter.default.removeObserver(observer)
-        }
+        self.observers.removeAll()
+
         if let monitor = self.eventMonitor {
             NSEvent.removeMonitor(monitor)
         }
